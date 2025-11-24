7342fa5f4e7243386de9689a5630f054ad1f0f6d
joelbahiajunior-jpg <joelbahiajunior@gmail.com>
2025-11-24 11:13:57 +0000

    atividade fixtures

---
+++ atividade_fixtures/test_soma.py
@@ -0,0 +1,19 @@
+import pytest
+
+def soma_dobro(numeros):
+    return sum(x*2 for x in numeros)
+
+@pytest.fixture
+def lista_numeros():
+    return [1,2,3,4,5]
+
+
+def test_soma_dobro(lista_numeros):
+    resultado=soma_dobro(lista_numeros)
+    assert resultado == 30
+
+def test_soma_dobro_vazia(lista_numeros):
+    lista_numeros.clear()
+    resultado=soma_dobro(lista_numeros)
+    assert resultado == 0
+
